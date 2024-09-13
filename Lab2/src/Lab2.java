041b0fcb946d8b9343517668a3198f32594b200e
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2024-09-13 14:58:44 +0200

    har typ gjort labben lol

--- Lab2/src/Lab2.java
+++ Lab2/src/Lab2.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.Random;
+
 
 /*
 1. Bob chooses secret primes p and q and computes n =pq.
@@ -15,22 +17,57 @@ public class Lab2 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        // to convert an integer b into a BigInteger
-        int b = 170;
-        BigInteger bigB = new BigInteger(String.valueOf(b));
+        // // to convert an integer b into a BigInteger
+        // int b = 170;
+        // BigInteger bigB = new BigInteger(String.valueOf(b));
 
-        // to read a string from keyboard
-        String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+        // // to read a string from keyboard
 
-        // to convert a string s into a BigInteger
-        String s = "abc";
-        BigInteger c = new BigInteger(s.getBytes());
+        // // to convert a string s into a BigInteger
+        // String s = "abc";
+        // BigInteger c = new BigInteger(s.getBytes());
 
-        System.out.println(input);
+        // System.out.println(input);
 
         // to convert a BigInteger back to a string
         // BigInteger a;
         // String s = new String(a.toByteArray());
+
+        int bitlength = 100;
+
+        BigInteger p = BigInteger.probablePrime(bitlength, new Random());
+        BigInteger q = BigInteger.probablePrime(bitlength, new Random());
+
+        BigInteger n = p.multiply(q);
+
+        BigInteger gcdNumber = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+
+        BigInteger e = gcdNumber.subtract(BigInteger.ONE);
+
+        while(!(e.gcd(gcdNumber).equals(BigInteger.ONE))){
+            e.subtract(BigInteger.ONE);
+        }
+
+        BigInteger d = e.modInverse(gcdNumber);
+
+        System.out.println("Please enter what you want to encrypt:");
+
+        String messageString = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+
+        BigInteger messageNumber = new BigInteger(messageString.getBytes());
+
+        BigInteger cipherNumber = messageNumber.modPow(e, n);
+
+        System.out.println(cipherNumber);
+
+        BigInteger decodedNumber = cipherNumber.modPow(d, n);
+
+        String decodedMessage = new String(decodedNumber.toByteArray());
+
+        System.out.println(decodedMessage);
+
+
+
 
     }
 }
