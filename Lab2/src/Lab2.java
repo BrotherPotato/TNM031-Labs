cedb98ce09bee8a1dac5112fc4ef68fef0e63771
julKara <julia021216@gmail.com>
2024-09-16 10:27:41 +0200

    Julia's comments

--- Lab2/src/Lab2.java
+++ Lab2/src/Lab2.java
@@ -33,37 +33,54 @@ public class Lab2 {
         // BigInteger a;
         // String s = new String(a.toByteArray());
 
+        /*************Generating keys, steps 1-4 lecture 6, slide 53*************/
         int bitlength = 100;
 
+        // 1. Generate large secrete primes p and q and compute n
         BigInteger p = BigInteger.probablePrime(bitlength, new Random());
         BigInteger q = BigInteger.probablePrime(bitlength, new Random());
 
-        BigInteger n = p.multiply(q);
+        BigInteger n = p.multiply(q);   // n = pq
 
-        BigInteger gcdNumber = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+        // 2. Generate e so that GCD(e, (p-1)(q-1)) = 1
+        BigInteger gcdNumber = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));   // GCD = (p-1)(q-1)
 
-        BigInteger e = gcdNumber.subtract(BigInteger.ONE);
+        BigInteger e = gcdNumber.subtract(BigInteger.ONE);  // e = GCD-1 (only to inichiate e)
 
-        while(!(e.gcd(gcdNumber).equals(BigInteger.ONE))){
+        // Make sure that e and GCD-number in co-prime (no common factors)
+        while(!(e.gcd(gcdNumber).equals(BigInteger.ONE))){  // Subtract with one until finding biggest common divisor (common factor)
             e.subtract(BigInteger.ONE);
         }
 
+        // 3. Compute d so that ed mod((p-1)(q-1)) = 1, move around so that d is alone
         BigInteger d = e.modInverse(gcdNumber);
 
-        System.out.println("Please enter what you want to encrypt:");
+        // 4. Make n and e public, those are the public keys
+
+
+        /*************Encrypt message, step 5 lecture 6, slide 53*************/
+        
+        System.out.println("Please enter what you want to encrypt:");   // Message to encrypts
 
         String messageString = (new BufferedReader(new InputStreamReader(System.in))).readLine();
 
         BigInteger messageNumber = new BigInteger(messageString.getBytes());
 
+        // Encryption
         BigInteger cipherNumber = messageNumber.modPow(e, n);
 
+        // Write out cipher, skip in reality, just send it to other person
         System.out.println(cipherNumber);
 
+        
+        /*************Decrypt message, step 6 lecture 6, slide 53*************/
+
+        // Decryption
         BigInteger decodedNumber = cipherNumber.modPow(d, n);
 
         String decodedMessage = new String(decodedNumber.toByteArray());
 
+        // Write out decrypted message
         System.out.println(decodedMessage);
     }
 }
