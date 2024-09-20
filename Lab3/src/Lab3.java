1df7ae1ac407717c97d6ee5fa43ed926dde06501
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2024-09-20 15:56:40 +0200

    saker

--- Lab3/src/Lab3.java
+++ Lab3/src/Lab3.java
@@ -1,5 +1,10 @@
+import javax.net.SocketFactory;
+import javax.net.*;
+import java.net.*; // behöver inte?
+
 public class Lab3 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        SocketFactory asd = null;
     }
 }
